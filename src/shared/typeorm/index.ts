c3929fa8ec9c9c218cfa69fb7960779b844b685d
Mariano-SI <Mariano.silva@ufvjm.edu.br>
2025-01-21 13:47:25 -0300

    feat: adaptacao do router para receber novas rotas de produtos

--- src/shared/typeorm/index.ts
+++ src/shared/typeorm/index.ts
@@ -1,3 +1,3 @@
 import { createConnection } from "typeorm";
 
-createConnection().then(() => console.log("Database connected!"));
+createConnection().then(() => console.log("Database connected!")).catch(() => console.log("Erro ao conectar no banco de dados"));
