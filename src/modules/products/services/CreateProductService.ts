192513aca424dce220e458e0c01c35a4e8d2c5b8
Mariano-SI <Mariano.silva@ufvjm.edu.br>
2025-01-21 11:10:27 -0300

    service para criação de produtos

---
+++ src/modules/products/services/CreateProductService.ts
@@ -0,0 +1,26 @@
+import { getCustomRepository } from "typeorm"
+import ProductRepository from "../typeorm/repositories/ProductsRepository"
+import AppError from "@shared/errors/AppError";
+import Product from "../typeorm/entities/Product";
+
+interface IRequest {
+  name: string,
+  price: number,
+  quantity: number
+}
+
+export default class CreateProductService{
+  public async execute({name, price, quantity}: IRequest): Promise<Product>{
+    const productsRepository = await getCustomRepository(ProductRepository);
+
+    const productAlreadyExists = await productsRepository.findByName(name);
+
+    if(productAlreadyExists){
+      throw new AppError('O produto já existe', 409)
+    }
+
+    const product = productsRepository.create({name, price, quantity});
+    const createdProduct = await productsRepository.save(product);
+    return createdProduct;
+  }
+}
