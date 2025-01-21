79fc69a1365083a3846782cef6b2b80aa7392eae
Mariano-SI <Mariano.silva@ufvjm.edu.br>
2025-01-21 11:33:26 -0300

    service para atualizacao de produtos

---
+++ src/modules/products/services/UpdateProductService.ts
@@ -0,0 +1,35 @@
+import { getCustomRepository } from "typeorm";
+import ProductRepository from "../typeorm/repositories/ProductsRepository";
+import { UUID } from "crypto";
+import AppError from "@shared/errors/AppError";
+
+interface IRequest{
+  id: UUID,
+  name: string,
+  price: number,
+  quantity: number
+}
+export default class UpdateProductService{
+  public async execute({id, name, price, quantity}:IRequest){
+    const productsRepository = getCustomRepository(ProductRepository);
+
+    const productToUpdate = await productsRepository.findOne(id);
+
+    if(!productToUpdate){
+      throw new AppError('Produto não encontrado', 404);
+    }
+
+    const nameAlreadyExists = await productsRepository.findByName(name);
+
+    if(nameAlreadyExists){
+      throw new AppError('Já existe um produto com esse nome', 409);
+    }
+
+    productToUpdate.name = name;
+    productToUpdate.price = price;
+    productToUpdate.quantity = quantity;
+
+    return await productsRepository.save(productToUpdate)
+
+  }
+}
