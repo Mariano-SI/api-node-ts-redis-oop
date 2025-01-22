c8ea6af3ee8f03178aa24de9c19f3b0b5caef1f0
Mariano-SI <Mariano.silva@ufvjm.edu.br>
2025-01-22 14:40:08 -0300

    service para exibir perfil de um usuario

---
+++ src/modules/users/services/ShowUserService.ts
@@ -0,0 +1,21 @@
+import { getCustomRepository } from "typeorm";
+import User from "../typeorm/entities/User";
+import UsersRepository from "../typeorm/repositories/UsersRepository";
+import AppError from "@shared/errors/AppError";
+
+
+interface IRequest{
+  id: string
+}
+
+export default class ShowUserService{
+  public async execute({id}: IRequest): Promise<User>{
+    const usersRepository = getCustomRepository(UsersRepository);
+    const user = await usersRepository.findById(id);
+
+    if(!user){
+      throw new AppError('Usuário não encontrado');
+    }
+    return user;
+  }
+}
