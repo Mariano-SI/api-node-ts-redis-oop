902c6378bda5a4d92451f830c214d3edbfbb1789
Mariano-SI <Mariano.silva@ufvjm.edu.br>
2025-01-23 14:58:11 -0300

    migracao para criicacao da coluna order_id na tabela pivo order_products

---
+++ src/shared/typeorm/migrations/1737654856036-AddOrderIdToOrdersProducts.ts
@@ -0,0 +1,28 @@
+import {MigrationInterface, QueryRunner, TableColumn, TableForeignKey} from "typeorm";
+
+export class AddOrderIdToOrdersProducts1737654856036 implements MigrationInterface {
+
+    public async up(queryRunner: QueryRunner): Promise<void> {
+      await queryRunner.addColumn('orders_products', new TableColumn({
+        name: 'order_id',
+        type: 'uuid',
+        isNullable: true
+      }));
+
+      await queryRunner.createForeignKey('orders_products', new TableForeignKey(
+        {
+          name: 'OrdersProductsOrder',
+          columnNames: ['order_id'],
+          referencedTableName:'orders',
+          referencedColumnNames:['id'],
+          onDelete: 'SET NULL'
+        }
+      ))
+    }
+
+    public async down(queryRunner: QueryRunner): Promise<void> {
+      await queryRunner.dropForeignKey('orders_products', 'OrdersProductsOrder');
+      await queryRunner.dropColumn('orders', 'order_id');
+    }
+
+}
