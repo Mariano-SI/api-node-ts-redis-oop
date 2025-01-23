346759f6eb81874fe5340e9fc6533433c52e7b2f
Mariano-SI <Mariano.silva@ufvjm.edu.br>
2025-01-23 19:27:24 -0300

    controler para modulo de pedidos

---
+++ src/modules/orders/controllers/OrdersController.ts
@@ -0,0 +1,30 @@
+import { Request, Response } from "express";
+import ShowOrderService from "../services/ShowOrderService";
+import CreateOrderService from "../services/CreateOrderService";
+
+
+export default class OrdersController{
+
+  public async show(req:Request, res: Response){
+    const {id} = req.params;
+
+    const showOrderService = new ShowOrderService();
+    const order = await showOrderService.execute({id});
+
+    return res.status(200).json(order);
+  }
+
+  public async create(req:Request, res: Response){
+    const {customerId, products} = req.body;
+
+
+    const createOrderService = new CreateOrderService();
+    const order = await createOrderService.execute({
+      customer_id: customerId,
+      products
+    });
+
+    return res.status(201).json(order);
+  }
+
+}
